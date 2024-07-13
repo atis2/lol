34e4e79013147b89fdeb0b0991d4d1a4e96b29dd
Atisd <atis.akimovs@gmail.com>
2024-07-14 00:31:40 +0300

    pro

--- bot.py
+++ bot.py
@@ -53,6 +53,7 @@ def ban_user(message):
 def make_some(message):
     bot.send_message(message.chat.id, 'I accepted a new user!')
     bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+    bot.send_sticker(message.chat_id, "CAACAgIAAxkBAAEsjtBmkvHdZXOZgGyhXyXGT0T7wdRjhAACzzYAAuFwuErvUaEly7Nz0TUE")
 
 
 bot.infinity_polling(none_stop=True)
